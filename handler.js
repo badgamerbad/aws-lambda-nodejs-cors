91e1361f6cda94169a4fe4e83ecdf696478a51a3
Vipul Dessai <badgamerbad@gmail.com>
2019-10-26 16:33:23 +0530

    post auth function added

--- handler.js
+++ handler.js
@@ -1,22 +1,55 @@
 'use strict';
+const request = require('request');
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-    },
-    body: JSON.stringify(
-      {
-        message: 'foo!',
-        input: event,
+const config = {
+    clientId: 'Iv1.c6778b1c26a766bd',
+    clientSecret: '24ee9c042bafc74699ff49270b0403da31e7ce30',
+    redirectUri: 'http://localhost:8000/',
+    allowedOrigins: ['http://localhost:8000/', 'https://localhost:8000/'],
+};
+
+module.exports.githutAccessTokenGenerator = async (event, context, callback) => {
+  // Retrieve the request, more details about the event variable later
+  const headers = event.headers;
+  const body = JSON.parse(event.body);
+  const origin = headers.Origin;
+
+  // Check for malicious request
+  if (!config.allowedOrigins.includes(origin)) {
+      throw new Error(`${origin} is not an allowed origin.`);
+  }
+
+  const url = 'https://github.com/login/oauth/access_token';
+  const options = {
+      headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
       },
-      null,
-      2
-    ),
+      body: JSON.stringify({
+          code: body.code,
+          client_id: config.clientId,
+          client_secret: config.clientSecret,
+          redirect_uri: config.redirectUri,
+          state: body.state,
+      }),
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  // Request to GitHub with the given code
+  request(url, options, function(err, response) {
+      if (err) {
+          callback({ "statusCode": 500, error: err });
+          return;
+      }
+
+      callback(null, {
+          "statusCode": 200,
+          "headers": {
+              'Access-Control-Allow-Origin': '*',
+              'Access-Control-Allow-Credentials': true,
+          },
+          // Access token should be stored in response.body
+          "body": JSON.stringify(response.body),
+          "isBase64Encoded": false
+      });
+  });
 };
