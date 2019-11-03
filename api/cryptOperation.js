010464eb304f892ec79c0e9c27544b521ecbef91
Vipul Dessai <badgamerbad@gmail.com>
2019-11-03 23:54:31 +0530

    added aes-256-gcm cipher

--- api/cryptOperation.js
+++ api/cryptOperation.js
@@ -3,7 +3,7 @@ const util = require("util");
 
 // Nodejs encryption with CTR
 const crypto = require('crypto');
-const algorithm = 'aes-256-cbc';
+const algorithm = 'aes-256-gcm';
 const iv = crypto.randomBytes(16);
 
 crypto.randomBytes = util.promisify(crypto.randomBytes);
@@ -11,8 +11,9 @@ crypto.randomBytes = util.promisify(crypto.randomBytes);
 const encrypt = async githubAccessToken => {
     const buffer = await crypto.randomBytes(48);
     const token = buffer.toString('hex');
-
-    let cipher = crypto.createCipheriv(algorithm, githubAccessToken, iv);
+    console.log("token-------------------------------------------")
+    console.log(token)
+    let cipher = crypto.createCipheriv(algorithm, Buffer.from(githubAccessToken.substr(0, 32)), iv);
     let encrypted = cipher.update(token);
     encrypted = Buffer.concat([encrypted, cipher.final()]);
     return encrypted.toString('hex');
@@ -20,11 +21,23 @@ const encrypt = async githubAccessToken => {
    
 const decrypt = async (token, githubAccessToken) => {
     let encryptedText = Buffer.from(token, 'hex');
-    let decipher = crypto.createDecipheriv(algorithm, githubAccessToken, iv);
+    let decipher = crypto.createDecipheriv(algorithm, Buffer.from(githubAccessToken.substr(0, 32)), iv);
     let decrypted = decipher.update(encryptedText);
     decrypted = Buffer.concat([decrypted, decipher.final()]);
     return decrypted.toString();
 }
 
-const encryptedToken = encrypt("57645173062d8e8faed8bc7a4b487c146989a8d5");
-// const boolean = decrypt(encryptedToken, "57645173062d8e8faed8bc7a4b487c146989a8d5");
+let encPromise = encrypt("57645173062d8e8faed8bc7a4b487c146989a8d5");
+encPromise.then(encString =>{ 
+    let decPromise = decrypt(encString, "57645173062d8e8faed8bc7a4b487c146989a8d5");
+    decPromise.then(decToken => {
+        console.log("decToken-------------------------------------------")
+        console.log(decToken)
+    })
+    .catch(err => {
+        console.log(err);
+    })
+})
+.catch(error => {
+    console.log(error);
+});
